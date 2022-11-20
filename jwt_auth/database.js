4327006f6b8b64d194ac60c76cc0500da039db2a
MaksGovor <maksgovruha@gmail.com>
2022-11-20 17:34:07 +0200

    Fix: add eslint & prettier

---
+++ jwt_auth/database.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const users = [
+  {
+    login: 'Login',
+    password: 'Password',
+    username: 'Username',
+  },
+  {
+    login: 'Login1',
+    password: 'Password1',
+    username: 'Username1',
+  },
+];
+
+module.exports = {
+  users,
+};
